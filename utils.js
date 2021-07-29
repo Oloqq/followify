b50f333e77b32dcb120fc404b92f11a632d5ccb2
Oloqq <olgierdpk@gmail.com>
2021-07-29 23:30:08 +0200

    Document chunkify

--- utils.js
+++ utils.js
@@ -1,4 +1,10 @@
 module.exports = {
+  /**
+   * Split input array into smaller chunks
+   * @param {*[]} arr input array
+   * @param {number} size size of the chunk
+   * @returns {[[]]} Array of chunks
+   */
   chunkify: function(arr, size) {
     chunks = [];
     chunkNum = 0;
