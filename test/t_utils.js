1f9830a8c2f7e4a32a8fbddc35fc2cf9d3e821e4
Oloqq <olgierdpk@gmail.com>
2021-07-30 01:49:49 +0200

    add first tests

---
+++ test/t_utils.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var utils = require('../utils')
+
+describe('utils', function() {
+  describe('#chunkify()', function() {
+    let chunkify = utils.chunkify;
+    it('works when actual length is a multiple of passed size', function() {
+      let res = chunkify([1, 2, 3, 4], 2)
+      let exp = [[1, 2], [3, 4]];
+      assert.deepStrictEqual(res, exp);
+    });
+    it('works when actual length is NOT amultiple of passed size', function() {
+      let res = chunkify([1, 2, 3, 4], 3)
+      let exp = [[1, 2, 3], [4]];
+      assert.deepStrictEqual(res, exp);         
+    });
+  });
+});
